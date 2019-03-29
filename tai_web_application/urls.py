e21a4ed910333ad767564b5df5ebbfc6a5267ae9
WKD622 <kuba.ziarko@gmail.com>
2019-03-29 17:50:43 +0100

    urls adds2

--- tai_web_application/urls.py
+++ tai_web_application/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
+from vehicles_sharing import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('vehicles_sharing/', include(urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

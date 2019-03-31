3950be5a0418344a34e027f7b62fee6e69d3318d
WKD622 <kuba.ziarko@gmail.com>
2019-03-31 16:04:04 +0200

    add reservation and user

--- vehicles_sharing/models.py
+++ vehicles_sharing/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    login = models.CharField(max_length=11)
+    password = models.CharField(max_length=50, null=False)
+    mail = models.CharField(null=False)
+    photo = models.BinaryField(null=True)
+
+
+class Reservation(models.Model):
+    owner_id = models.IntegerField()
+    client_id = models.IntegerField()
+    car_id = models.IntegerField()
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
+    active = models.BooleanField(default=False)
+    message = models.CharField(max_length=3000)

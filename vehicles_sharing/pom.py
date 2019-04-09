192d965aa2665e1b6dba73dd971174c10a16e281
Jakub Ziarko <kuba.ziarko@gmail.com>
2019-04-09 20:57:14 +0200

    refactoring

---
+++ vehicles_sharing/pom.py
@@ -0,0 +1,59 @@
+import re
+from django.db.models import Q
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+
+class PomMethods:
+    @staticmethod
+    def get_user_from_token(request):
+        token = request.META.get('HTTP_AUTHORIZATION').split(" ")[1]
+        if token:
+            user_id = Token.objects.get(key=token).user_id
+            return User.objects.get(id=user_id)
+        else:
+            return None
+
+    @staticmethod
+    def check_if_params_correct(self, params):
+        """TODO"""
+        pass
+
+    @staticmethod
+    def normalize_query(query_string,
+                        findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                        normspace=re.compile(r'\s{2,}').sub):
+        return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
+    @staticmethod
+    def get_query(query_string, search_fields):
+        query = None
+        terms = PomMethods.normalize_query(query_string)
+        for term in terms:
+            or_query = None
+            for field_name in search_fields:
+                q = Q(**{"%s__icontains" % field_name: term})
+                if or_query is None:
+                    or_query = q
+                else:
+                    or_query = or_query | q
+            if query is None:
+                query = or_query
+            else:
+                query = query & or_query
+        return query
+
+
+class FilteringParams:
+    MIN_PRICE = 'min_price'
+    MAX_PRICE = 'max_price'
+    MIN_POWER = 'min_power'
+    MAX_POWER = 'max_power'
+    CITY = 'city'
+    BRAND = 'brand'
+    MODEL = 'model'
+    MIN_PRODUCTION_YEAR = 'min_production_year'
+    MAX_PRODUCTION_YEAR = 'min_production_year'
+    MIN_CAPACITY = 'min_capacity'
+    MAX_CAPACITY = 'max_capacity'
+    DRIVE_TRAIN = 'drive_train'
--- vehicles_sharing/views.py
+++ vehicles_sharing/views.py
@@ -1,30 +1,14 @@
 from django.contrib.auth.models import User
 from django_filters import filters
 from rest_framework import viewsets, status
-from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
 from rest_framework.response import Response
-import re
-from django.db.models import Q
 
 from .models import Vehicle
+from .pom import PomMethods as pm
 from .serializers import UserSerializer
 from .serializers import VehicleSerializer
-
-
-class FilteringParams:
-    MIN_PRICE = 'min_price'
-    MAX_PRICE = 'max_price'
-    MIN_POWER = 'min_power'
-    MAX_POWER = 'max_power'
-    CITY = 'city'
-    BRAND = 'brand'
-    MODEL = 'model'
-    MIN_PRODUCTION_YEAR = 'min_production_year'
-    MAX_PRODUCTION_YEAR = 'min_production_year'
-    MIN_CAPACITY = 'min_capacity'
-    MAX_CAPACITY = 'max_capacity'
-    DRIVE_TRAIN = 'drive_train'
+from .pom import FilteringParams
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -38,21 +22,8 @@ class VehicleViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.OrderingFilter,)
     ordering_fields = ('price', 'power', 'production_year')
 
-    @staticmethod
-    def _get_user_from_token(request):
-        token = request.META.get('HTTP_AUTHORIZATION').split(" ")[1]
-        if token:
-            user_id = Token.objects.get(key=token).user_id
-            return User.objects.get(id=user_id)
-        else:
-            return None
-
-    def _check_if_params_correct(self, params):
-        """TODO"""
-        pass
-
     def create(self, request, *args, **kwargs):
-        user = self._get_user_from_token(request)
+        user = pm.get_user_from_token(request)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(owner_id=user)
@@ -62,7 +33,7 @@ class VehicleViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         url_parameters = request.GET
         vehicles = Vehicle.objects.all()
-        self._check_if_params_correct(url_parameters)
+        pm.check_if_params_correct(url_parameters)
         for key, value in url_parameters.items():
             if key == FilteringParams.MIN_PRICE:
                 vehicles = vehicles.filter(price__gte=value)
@@ -89,44 +60,15 @@ class VehicleViewSet(viewsets.ModelViewSet):
             elif key == FilteringParams.DRIVE_TRAIN:
                 vehicles = vehicles.filter(drive_train=value)
 
-        # page = self.paginate_queryset(queryset)
-        # if page is not None:
-        #     serializer = self.get_serializer(page, many=True)
-        #     return self.get_paginated_response(serializer.data)
-
         serializer = self.get_serializer(vehicles, many=True)
         return Response(serializer.data)
 
-    @staticmethod
-    def _normalize_query(query_string,
-                         findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
-                         normspace=re.compile(r'\s{2,}').sub):
-        return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
-
-    def _get_query(self, query_string, search_fields):
-        query = None
-        terms = self._normalize_query(query_string)
-        for term in terms:
-            or_query = None
-            for field_name in search_fields:
-                q = Q(**{"%s__icontains" % field_name: term})
-                if or_query is None:
-                    or_query = q
-                else:
-                    or_query = or_query | q
-            if query is None:
-                query = or_query
-            else:
-                query = query & or_query
-        return query
-
     @action(detail=False, methods=['GET'])
     def search(self, request, *args, **kwargs):
-        query_string = ''
         found_entries = None
         if ('on' in request.GET) and request.GET['on'].strip():
             query_string = request.GET['on']
-            entry_query = self._get_query(query_string, ['brand', 'model'])
+            entry_query = pm.get_query(query_string, ['brand', 'model'])
             found_entries = Vehicle.objects.filter(entry_query)
 
         serializer = self.get_serializer(found_entries, many=True)
